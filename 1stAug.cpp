1d55cbffade35efa376498b54ed47279bdb42cad
Rohan Belwal <54302883+belwalrohan@users.noreply.github.com>
2020-08-12 02:41:28 -0700

    Create 1stAug.cpp

---
+++ 1stAug.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word[0]>='a' && word[0]<='z')
+        {
+            for(int i=1;i<word.size();i++)
+            {
+                if(!(word[i]>='a' && word[i]<='z'))
+                    return false;
+            }
+        }
+        else
+        {
+            if(word[1]>='A' && word[1]<='Z')
+            {
+                for(int i=2;i<word.size();i++)
+                {
+                    if(!(word[i]>='A' && word[i]<='Z'))
+                        return false;
+                }
+            }
+            else
+            {
+                for(int i=2;i<word.size();i++)
+                {
+                    if(!(word[i]>='a' && word[i]<='z'))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
